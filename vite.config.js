a839177cb8fa2c9fa9fd63fadebdee69851c0478
M Atif <chatif476@gmail.com>
2023-10-06 01:15:43 +0500

    SEO

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import Sitemap from "vite-plugin-sitemap";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Sitemap()],
   server: {
     host: true,
   },
