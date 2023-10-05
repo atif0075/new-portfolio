f0be75978003f476bd531ef49801299adb0dfc1a
M Atif <chatif476@gmail.com>
2023-10-06 01:18:53 +0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import vue from "@vitejs/plugin-vue";
 import Sitemap from "vite-plugin-sitemap";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Sitemap()],
+  plugins: [
+    vue(),
+    Sitemap({
+      hostname: "https://connectsatif.vercel.app/",
+    }),
+  ],
   server: {
     host: true,
   },
